7acc113be3a6870de22b7ceb9f52eb412e2ce322
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-04-06 18:07:19 -0500

    update

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -1,5 +1,52 @@
+
 package server;
 
-public class ServerThread {
+import java.io.IOException;
+import java.net.*;
+
+public class ServerThread extends Thread{
+	private DatagramPacket receivePacket;
+	private byte[] sendData;
+	private DatagramSocket serverSocket;
+	private byte[] receiveData;
+	private InetAddress IPAddress;
+	private int PORT;
+	public ServerThread (DatagramPacket receivePacket, byte[] sendData, DatagramSocket serverSocket, byte[] receiveData, InetAddress IPAddress, int PORT){
+		this.receivePacket= receivePacket;
+		this.receiveData= receiveData;
+		this.sendData = sendData;
+		this.serverSocket= serverSocket;
+		this.IPAddress=IPAddress;
+		this.PORT=PORT;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		System.out.println("packet received");
+
+		String sentence = new String(receiveData);
+		//String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+		System.out.println(sentence);
+		//InetAddress IPAddress = receivePacket.getAddress();
+		System.out.println("get address " + IPAddress );
+		//int port = receivePacket.getPort();
+		System.out.println("get port " + PORT);
+		String capitalizedSentence = sentence.toUpperCase();
+		sendData = capitalizedSentence.getBytes();
+		DatagramPacket sendPacket =
+				new DatagramPacket(sendData, sendData.length, IPAddress,
+						PORT);
+		try {
+			serverSocket.send(sendPacket);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
--- src/server/ServerUDP.java
+++ src/server/ServerUDP.java
@@ -1,5 +1,28 @@
 package server;
+import java.io.*;
+import java.net.*;
+class ServerUDP extends Thread {
 
-public class ServerUDP {
+	static DatagramSocket serverSocket;
+
+
+	public static void main(String args[]) throws Exception
+	{
+		serverSocket = new DatagramSocket(5000);
+		// TODO Auto-generated method stub
+		byte[] receiveData = new byte[1024];
+		byte[] sendData = new byte[1024];
+		DatagramPacket receivePacket =
+				new DatagramPacket(receiveData, receiveData.length);
+		System.out.println("before receive packet");
+
+		while(true)
+
+		{
+			serverSocket.receive(receivePacket);
+			System.out.println(new String(receivePacket.getData()) + ":: data receive packet");
+			new ServerThread(receivePacket, sendData, serverSocket, receivePacket.getData(), receivePacket.getAddress(), receivePacket.getPort()).start();
+		}
+	}
 
 }
