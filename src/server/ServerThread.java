93b22a0d5d2a7121f8ff6688d79c68a983151122
Laura <l.castro12@uniandes.edu.co>
2017-04-07 23:30:52 -0500

    Servidor puede guardar archivo

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -1,8 +1,24 @@
 
 package server;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.net.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+
 
 public class ServerThread extends Thread{
 	private DatagramPacket receivePacket;
@@ -11,20 +27,70 @@ public class ServerThread extends Thread{
 	private byte[] receiveData;
 	private InetAddress IPAddress;
 	private int PORT;
-	public ServerThread (DatagramPacket receivePacket, byte[] sendData, DatagramSocket serverSocket, byte[] receiveData, InetAddress IPAddress, int PORT){
+	public boolean nuevo;
+	public String content;
+	public ServerThread (DatagramPacket receivePacket, byte[] sendData, DatagramSocket serverSocket, byte[] receiveData, InetAddress IPAddress, int PORT, boolean nuevo,String content){
 		this.receivePacket= receivePacket;
 		this.receiveData= receiveData;
 		this.sendData = sendData;
 		this.serverSocket= serverSocket;
 		this.IPAddress=IPAddress;
 		this.PORT=PORT;
+		this.nuevo=nuevo;
+		this.content=content;
+		
+
+
 	}
 	
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		
 		super.run();
 		System.out.println("packet received");
+		String cliente = IPAddress + "," + PORT + "";
+		
+		try{
+			
+			byte[] contentInBytes = content.getBytes();
+			if(nuevo){
+				
+				try {
+					FileOutputStream fos = new FileOutputStream(new File("./data",cliente + ".txt"));
+					BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+					bos.write(contentInBytes);
+					System.out.println("-------PUDO--------");
+					bos.flush();
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			else{
+				try(    
+					FileWriter fw = new FileWriter("./data"+cliente + ".txt", true);
+					BufferedWriter bw = new BufferedWriter(fw);
+					PrintWriter out = new PrintWriter(bw)){
+					out.println("\n"+content);
+					out.close();
+					bw.close();
+					fw.close();
+				}  
+				catch( IOException e ){
+					// File writing/opening failed at some stage.
+				}
+
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
 
 		String sentence = new String(receiveData);
 		//String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
@@ -33,20 +99,11 @@ public class ServerThread extends Thread{
 		System.out.println("get address " + IPAddress );
 		//int port = receivePacket.getPort();
 		System.out.println("get port " + PORT);
-		String capitalizedSentence = sentence.toUpperCase();
-		sendData = capitalizedSentence.getBytes();
-		DatagramPacket sendPacket =
-				new DatagramPacket(sendData, sendData.length, IPAddress,
-						PORT);
-		try {
-			serverSocket.send(sendPacket);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+
 	}
+
 	
-	
+
 
 }
--- src/server/ServerUDP.java
+++ src/server/ServerUDP.java
@@ -1,27 +1,70 @@
 package server;
 import java.io.*;
 import java.net.*;
-class ServerUDP extends Thread {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+class ServerUDP {
 
 	static DatagramSocket serverSocket;
-
-
+	static boolean nuevo;
+	static ArrayList<String> clientes;
+	
+	private static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
+		long diffInMillies = date2.getTime() - date1.getTime();
+		return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+	}
 	public static void main(String args[]) throws Exception
 	{
-		serverSocket = new DatagramSocket(5000);
 		// TODO Auto-generated method stub
+		//int port= Integer.parseInt(args[0]);
+
+		serverSocket = new DatagramSocket(5000);
 		byte[] receiveData = new byte[1024];
 		byte[] sendData = new byte[1024];
-		DatagramPacket receivePacket =
-				new DatagramPacket(receiveData, receiveData.length);
 		System.out.println("before receive packet");
+		nuevo=true;
+		clientes=new ArrayList<String>();
+		
 
 		while(true)
 
 		{
+			DatagramPacket receivePacket =
+					new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
 			System.out.println(new String(receivePacket.getData()) + ":: data receive packet");
-			new ServerThread(receivePacket, sendData, serverSocket, receivePacket.getData(), receivePacket.getAddress(), receivePacket.getPort()).start();
+			System.out.println(receivePacket.getPort());
+			String cliente= receivePacket.getAddress() + "," + receivePacket.getPort();
+			if(clientes.size()!=0){
+				for(int i=0;i<clientes.size()&& nuevo;i++){
+					System.out.println(clientes.get(i));
+					System.out.println(cliente);
+					if(cliente.equals(clientes.get(i))){
+						nuevo=false;
+					}
+					else{
+						nuevo=true;
+					}
+				}
+			}
+			
+			if(nuevo){
+				clientes.add(cliente);
+				
+			}
+			String linea = new String(receiveData);
+			String[] s=linea.split("=");
+			String seq = String.valueOf(s[1].charAt(0));
+			String fecha = s[2];
+			DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+			Date fechaSalida = df.parse(fecha);
+			Date actual = new Date();
+			long dif = getDateDiff(fechaSalida,actual,TimeUnit.MILLISECONDS);
+			String content = seq + ":" + dif + "ms";
+			new ServerThread(receivePacket, sendData, serverSocket, receivePacket.getData(), receivePacket.getAddress(), receivePacket.getPort(),nuevo,content).start();
 		}
 	}
 
